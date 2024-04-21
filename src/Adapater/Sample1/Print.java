b351624bae5d0efa65589ca062c2b7bed4dc77ea
Naoki Haba <naosprintrunner6385@gmail.com>
2024-04-22 01:36:50 +0900

    Add Adapter design pattern samples

---
+++ src/Adapater/Sample1/Print.java
@@ -0,0 +1,7 @@
+package Adapater.Sample1;
+
+public interface Print {
+    public abstract void printWeak();
+
+    public abstract void printStrong();
+}
---
+++ src/Adapater/Sample1/main.java
@@ -0,0 +1,9 @@
+package Adapater.Sample1;
+
+public class main {
+    public static void main(String[] args) {
+        Print p = new PrintBanner("Hello");
+        p.printWeak();
+        p.printStrong();
+    }
+}
